8ac8ad843c07f6885253ff01ed115943aa9b597d
last_arg <last_arg@tutanota.com>
2021-08-03 21:27:18 +0300

    search games from twitch

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,5 +1,7 @@
-import { Component, createResource, For } from 'solid-js';
+import { Component, createResource, createSignal, For } from 'solid-js';
 import Header from '../components/Header'
+
+// TODO: make game list element into component. Or make whole list (ul) into component
 
 const IMG_WIDTH = 143;
 const IMG_HEIGHT = 190;
@@ -11,36 +13,100 @@ interface Game {
 }
 
 const client_id = "7v5r973dmjp0nd1g43b8hcocj2airz";
+const HEADER_OPTS = {
+  method: "GET",
+  headers: {
+    "Host": "api.twitch.tv",
+    // TODO: remove hardcoded bearer token
+    "Authorization": `Bearer ${import.meta.env.VITE_TWITCH_ACCESS_TOKEN}`,
+    "Client-id": client_id,
+    "Accept": "application/vnd.twitchtv.v5+json",
+  }
+};
 
 const fetchTopGames = async (id): Promise<Game[]> => {
   const url = "https://api.twitch.tv/helix/games/top?first=10";
-  let opts = {
-    method: "GET",
-    headers: {
-      "Host": "api.twitch.tv",
-      // TODO: remove hardcoded bearer token
-      "Authorization": `Bearer ${import.meta.env.VITE_TWITCH_ACCESS_TOKEN}`,
-      "Client-id": client_id,
-      "Accept": "application/vnd.twitchtv.v5+json",
-    }
-  };
-
   if (import.meta.env.DEV) {
     return (await (await fetch("/tmp/top_games.json")).json()).data;
   } else {
-    return (await (await fetch(url, opts)).json()).data;
+    if (import.meta.env.VITE_TWITCH_ACCESS_TOKEN === undefined) {
+      throw "Three is no twitch access token solution setup for prodution site";
+    }
+    return (await (await fetch(url, HEADER_OPTS)).json()).data;
   }
 }
 
-const Home: Component = () => {
-  const [topGames] = createResource("topGames", fetchTopGames)
+const searchGames = async (search_term): Promise<Game[]> => {
+  const trimmed_term = search_term.trim();
+  if (trimmed_term.length === 0) {
+    return [];
+  }
+  const url = `https://api.twitch.tv/helix/search/categories?first=10&query=${trimmed_term}`;
+  return (await (await fetch(url, HEADER_OPTS)).json()).data;
+};
 
+const Home: Component = () => {
+  const searchHash = location.hash.length > 0 ? location.hash.slice(1) : "";
+  const [searchValue, setSearchValue] = createSignal(searchHash);
+  console.log(searchValue())
+  const [foundGames] = createResource(searchValue, searchGames);
+  const [topGames] = createResource(fetchTopGames);
+  let searchTimeout: number | null = null;
   return (
     <>
-      <Header/>
+      <header class="bg-gray-700 p-1 shadow flex">
+        <h1 class="text-white">Twitch filter</h1>
+        <form onSubmit={(e) => {
+            e.preventDefault();
+            clearTimeout(searchTimeout);
+            const value  = e.currentTarget.querySelector("input[type=search]").value;
+            setSearchValue(value);
+        }}>
+          <input
+            type="search"
+            class="border bg-blue-100"
+            placeholder="Search for game"
+            value={searchValue()}
+            onInput={(e) => {
+              clearTimeout(searchTimeout);
+              const value = e.currentTarget.value;
+              searchTimeout = setTimeout(() => {
+                setSearchValue(value)
+              }, 400);
+            }}
+          />
+          <button type="submit">Search</button>
+        </form>
+      </header>
       <main>
         <div>
           <h2>Search results</h2>
+          <ul class="flex flex-wrap">
+            <Switch>
+              <Match when={foundGames.loading} >
+                <p>Searching...</p>
+              </Match>
+              <Match when={searchValue().trim().length > 0 && foundGames().length === 0}>
+                <p>No results found</p>
+              </Match>
+              <Match when={foundGames().length > 0}>
+                <For each={foundGames()}>
+                  {(game) => {
+                      let img_url = game.box_art_url.replace("52x72", `${IMG_WIDTH}x${IMG_HEIGHT}`);
+                      const game_link = `/directory/game/${game.name}`;
+                      return (
+                        <li>
+                          <a href={game_link} title={game.name}>
+                            <img src={img_url} alt="" width={IMG_WIDTH} height={IMG_HEIGHT} />
+                          </a>
+                          <p>{game.name}</p>
+                        </li>
+                      );
+                  }}
+                </For>
+              </Match>
+            </Switch>
+          </ul>
         </div>
         <div>
           <h2>Top games</h2>
@@ -53,7 +119,7 @@ const Home: Component = () => {
                   return (
                     <li>
                       <a href={game_link} title={game.name}>
-                        <img src={img_url} alt="" />
+                        <img src={img_url} alt="" width={IMG_WIDTH} height={IMG_HEIGHT} />
                       </a>
                       <p>{game.name}</p>
                     </li>
