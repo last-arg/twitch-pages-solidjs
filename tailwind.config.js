97c07799442ffab719c4b4446afc78a37a489e5f
last_arg <last_arg@tutanota.com>
2021-08-09 13:03:51 +0300

    CategoryTitle link was too big

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors')
 
 module.exports = {
   mode: 'jit',
   purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors : colors,
     extend: {
       fontWeight: ['hover', 'focus'],
       fontFamily: {
