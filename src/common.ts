6aa5fce96cb0a812c51b5af83f00a24fd1ac0e96
last_arg <last_arg@tutanota.com>
2021-08-12 17:20:47 +0300

    clean code

--- src/common.ts
+++ src/common.ts
@@ -13,30 +13,25 @@ interface GameFollow {
 
 export const localGames = createMutable({
   games: JSON.parse(window.localStorage.getItem("games") ?? "[]") as GameFollow[],
-  get count(): number {
-    return this.games.length;
-  },
   get gameIds(): string[] {
     return this.games.map((game: GameFollow) => game.id)
   },
   isFollowed(id: string): boolean {
     return this.gameIds.includes(id)
   },
-  followGame(game: {id: string, name: string}) {
+  followGame(game: GameFollow) {
     this.games.push(game)
     window.localStorage.setItem("games", JSON.stringify(this.games));
   },
   unfollowGame(id: string) {
-    const index = this.games.findIndex((cat: GameFollow) => cat.id === id)
+    const index = this.gameIds.indexOf(id)
     if (index !== -1) {
-      // this.games = [...this.games.slice(0, index), ...this.games.slice(index+1)]
       this.games.splice(index, 1)
       window.localStorage.setItem("games", JSON.stringify(this.games));
     }
 
   }
 });
-
 
 export const createTwitchImage = (name: string, width: number, height: number): string => {
   return `https://static-cdn.jtvnw.net/ttv-boxart/${name}-${width}x${height}.jpg`;
