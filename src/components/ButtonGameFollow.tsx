b9ec01b351621c43407490947aae16ef5e4652bc
last_arg <last_arg@tutanota.com>
2021-08-15 11:54:38 +0300

    Attribute object will be made automatically into html element attribute list

--- src/components/ButtonGameFollow.tsx
+++ src/components/ButtonGameFollow.tsx
@@ -2,37 +2,33 @@ import { Show, Component, createSignal, createEffect } from 'solid-js';
 import { localGames } from '../common';
 import { IconFollow, IconUnfollow } from '../icons';
 
-type ButtonAttr = {title: string, "aria-pressed": "true" | "false"}
+type ButtonAttr = {title: string, "aria-pressed": boolean}
 
 const createButtonAttr = (id: string): ButtonAttr => {
   const isFollowed = localGames.isFollowed(id)
   const title = isFollowed ? "Unfollow game" : "Follow game"
-  const ariaPressed = isFollowed ? "true" : "false"
-  return { title, "aria-pressed": ariaPressed }
+  return { title, "aria-pressed": isFollowed }
 }
 
-const ButtonGameFollow: Component<{id: string, name: string, classExtra: string}> = (props) => {
-  const classExtra = props.classExtra || "";
+const ButtonGameFollow: Component<{id: string, name: string, classExtra: string = ""}> = (props) => {
+  const classExtra = props.classExtra;
   const [buttonAttr, setButtonAttr] = createSignal<ButtonAttr>(createButtonAttr(props.id))
   createEffect(() => setButtonAttr(createButtonAttr(props.id)))
 
   const toggleFollow = (category: {id: string, name: string}, e: MouseEvent) => {
     e.preventDefault();
-    if (buttonAttr()["aria-pressed"] === "true") {
+    if (buttonAttr()["aria-pressed"]) {
       localGames.unfollowGame(category.id)
     } else {
       localGames.followGame({id: category.id, name: category.name})
     }
   }
 
-  const t = {
-    "is-disabled": "test",
-    width: 10
-  }
-
   return (
     <button type="button" class={`text-trueGray-400 hover:text-black ${classExtra}`} onClick={[toggleFollow, {id: props.id, name: props.name}]} {...buttonAttr}>
-      <Show when={buttonAttr()["aria-pressed"] === "false"} fallback={<IconUnfollow />}><IconFollow /></Show>
+      <Show when={!buttonAttr()["aria-pressed"]} fallback={<IconUnfollow />}>
+        <IconFollow />
+      </Show>
     </button>
   );
 };
